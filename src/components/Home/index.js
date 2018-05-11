82c2fc0b2c0b47eab979aef3655555691668403a
Rich Garrick <richg341@gmail.com>
2018-05-11 16:53:02 -0700

    update css for parallax version

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,22 +5,53 @@ import BgVideo from "../BgVideo";
 class Home extends Component {
   render() {
     return (
-      <div>
+      <main >
       <BgVideo />
-    <section className="main">
-        <div class="rl-banner">
-          <div class="container">
-            <div class="ban ">
-                <div class="hello ">HELLO</div>
-                <div class="triangle"></div>
+        <section className="main">
+          <div className="rl-banner">
+            <div className="container">
+              <div className="ban ">
+                  <div className="hello ">HELLO</div>
+                  <div className="triangle"></div>
+              </div>
+                <h1 className="wow fadeInLeft" data-wow-duration="2s"> I'm <strong>Richard</strong></h1>
+                <h2 className="wow fadeInRight" data-wow-duration="2s" >Junior Software Developer</h2>
+                <div className="middle-btn"> <a href="/" className="btn btn-default wow fadeInUp" data-wow-duration="2s" >Hire Me Now</a> </div>
             </div>
-              <h1 class="wow fadeInLeft" data-wow-duration="2s"> I'm <strong>Richard</strong></h1>
-              <h2 class="wow fadeInRight" data-wow-duration="2s" >Junior Software Developer</h2>
-              <div class="middle-btn"> <a href="/" class="btn btn-default wow fadeInUp" data-wow-duration="2s" >Hire Me Now</a> </div>
+          </div>      
+        </section>
+        <section className="section parallax rl-about">
+          <div>
+            <p>test</p>
           </div>
-        </div>      
-      </section>
-      </div>
+        </section>
+        <section className="section parallax rl-skills">
+          <div>
+            <p>test</p>
+          </div>
+        </section>
+        <section className="section parallax rl-portfolio">
+          <div>
+            <p>test</p>
+          </div>
+        </section>
+        <section className="section parallax rl-experience">
+          <div>
+            <p>test</p>
+          </div>
+        </section>
+        <section className="section parallax rl-clients">
+          <div>
+            <p>test</p>
+          </div>
+        </section>
+        <section className="section parallax rl-contact">
+          <div>
+            <p>test</p>
+          </div>
+        </section>
+        
+      </main>
     );
   }
 }
